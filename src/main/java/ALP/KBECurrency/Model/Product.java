67e5948947b128403158245247b6f3c51fa23b7d
S0571740 <philip.blankenburg@student.htw-berlin.de>
2022-09-26 15:33:16 +0200

    adjust model and remove imports

--- src/main/java/ALP/KBECurrency/Model/Product.java
+++ src/main/java/ALP/KBECurrency/Model/Product.java
@@ -11,20 +11,33 @@ public class Product implements Serializable {
 
     private String name;
     private String additionalInfo;
+    private String preis;
     private List<Component> components = new LinkedList<>();
 
     public Product(String name,
             String additionalInfo,
             Component... components) {
-        this.name = name;
-        for(Component comp : components) {
+        for (Component comp : components) {
             this.components.add(comp);
         }
+        this.name = name;
+        this.additionalInfo = additionalInfo;
+    }
+
+    public Product(String name,
+            String additionalInfo,
+            String preis,
+            Component... components) {
+        for (Component comp : components) {
+            this.components.add(comp);
+        }
+        this.name = name;
+        this.preis = preis;
         this.additionalInfo = additionalInfo;
     }
 
     public Product() {
-        
+
     }
 
     public String getName() {
@@ -39,8 +52,16 @@ public class Product implements Serializable {
         return this.components;
     }
 
-    public String getAdditionalInfo(){
+    public String getAdditionalInfo() {
         return this.additionalInfo;
+    }
+
+    public String getPreis() {
+        return this.preis;
+    }
+
+    public void setPreis(String preis) {
+        this.preis = preis;
     }
 
     @Override
